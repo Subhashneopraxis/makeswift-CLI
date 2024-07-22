1859dc037ef5900fd326c6e9a124efd79589ea31
Subhashneopraxis <subash.surendran@neopraxis.in>
2024-07-22 15:23:13 +0530

    first commit

--- lib/makeswift/components.ts
+++ lib/makeswift/components.ts
@@ -2,3 +2,4 @@ import '~/components/ui/button/button.makeswift';
 import '~/components/carousel-static/carousel-static.makeswift';
 import '~/components/product-carousel/product-carousel.makeswift';
 import '~/components/ui/product-preview/product-preview.makeswift';
+import '~/components/ui/custom-product-components/custom-pdp.makeswift'
